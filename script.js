f85a7bf0dee01415c0c6ea815a2d42e008530958
MickyMCY <mickybrow22donodoscanais@gmail.com>
2026-02-19 11:23:45 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+   Standalone JavaScript for Checkout
+   Logic for timer, validation, and redirection
+*/
+const WHATSAPP_NUMBER = "+25884598917";
+const PRICE = "244 MZN";
+
+// All functions are embedded in standalone.html for single-file portability.
