2c4a5ff22c882bb8857bb4ec4eb8ebbba3bf0ab8
MickyMCY <mickybrow22donodoscanais@gmail.com>
2026-02-23 00:26:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+
 const screens = document.querySelectorAll(".screen");
 const btnStep1 = document.getElementById("btnStep1");
 const mpesaBtn = document.getElementById("mpesaBtn");
@@ -11,75 +13,62 @@ const toast = document.getElementById("toast");
 
 let numeroAtual = "";
 
-function vibrar(){
-if(navigator.vibrate) navigator.vibrate(100);
-}
-
 function goTo(step){
-screens.forEach(s=>s.classList.remove("active"));
+screens.forEach(s => s.classList.remove("active"));
 document.getElementById("screen"+step).classList.add("active");
 }
 
-btnStep1.addEventListener("click",()=>{
-const nome = document.getElementById("nome").value.trim();
+btnStep1.addEventListener("click", function(){
+
 const telefone = document.getElementById("telefone").value.replace(/\D/g,'');
 
 if(telefone.length !== 9){
-erroTelefone.innerText = telefone.length < 9 ? 
-"Faltam dígitos no número." : "Número inválido.";
-vibrar();
+erroTelefone.innerText="Número deve ter 9 dígitos.";
 return;
 }
 
-if(!["84","85","86","87"].includes(telefone.substring(0,2))){
-erroTelefone.innerText="Prefixo inválido.";
-vibrar();
-return;
-}
-
-erroTelefone.innerText="";
 goTo(2);
 });
 
-mpesaBtn.addEventListener("click",()=>{
+mpesaBtn.addEventListener("click", function(){
 numeroAtual="844459897";
 numeroPagamento.innerText=numeroAtual;
-ussdInfo.innerText="Marque *150# no seu telefone e efectue o pagamento.";
+ussdInfo.innerText="Marque *150# no seu telefone para pagar.";
 goTo(3);
-mostrarBotaoConfirmar();
+mostrarBotao();
 });
 
-emolaBtn.addEventListener("click",()=>{
+emolaBtn.addEventListener("click", function(){
 numeroAtual="867145774";
 numeroPagamento.innerText=numeroAtual;
-ussdInfo.innerText="Marque *898# no seu telefone e efectue o pagamento.";
+ussdInfo.innerText="Marque *898# no seu telefone para pagar.";
 goTo(3);
-mostrarBotaoConfirmar();
+mostrarBotao();
 });
 
-copyBtn.addEventListener("click",()=>{
+copyBtn.addEventListener("click", function(){
 navigator.clipboard.writeText(numeroAtual);
 copyBtn.innerText="copiado";
 copyBtn.style.transform="scale(.85)";
 toast.style.display="block";
-setTimeout(()=>{
-toast.style.display="none";
-},1500);
+setTimeout(()=>{toast.style.display="none";},1500);
 });
 
-function mostrarBotaoConfirmar(){
+function mostrarBotao(){
 confirmBtn.classList.add("hidden");
 setTimeout(()=>{
 confirmBtn.classList.remove("hidden");
 },15000);
 }
 
-confirmBtn.addEventListener("click",()=>{
+confirmBtn.addEventListener("click", function(){
 window.location.href="https://wa.me/25884598917";
 });
 
 document.querySelectorAll(".back").forEach(el=>{
-el.addEventListener("click",()=>{
+el.addEventListener("click", function(){
 goTo(el.dataset.back);
 });
+});
+
 });
